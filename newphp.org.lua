116867887b2546ed96a29e77c8a79fb413f9833b
newphp@gmail.com <KunTian>
2012-11-13 04:51:26 +0800

    update

--- newphp.org.lua
+++ newphp.org.lua
@@ -3,10 +3,13 @@
 -- _a = "newphp.org"
 
 -- A Records
-a(_a, "1.2.3.4")
+a(_a, "182.237.3.102")
+a("contacts", "182.237.3.102")
+a("fetion", "118.122.178.64")
+a("overdue", "118.122.178.64")
 
 -- CNAME Records
 cname("www", _a)
 
 -- MX Records
-mx(_a, _a)
+#mx(_a, _a)
