c8169fe4873357e45f25b415cdc47acab3d39f5d
newphp <newphp@users.noreply.github.com>
2014-11-06 20:12:58 +0800

    Create templates.lua

---
+++ templates.lua
@@ -0,0 +1,11 @@
+function google_mail(domain)
+  -- Configure Google Apps mail exchangers
+  mx(domain, "aspmx.l.google.com", 5)
+  mx(domain, "alt1.aspmx.l.google.com", 10)
+  mx(domain, "alt2.aspmx.l.google.com", 10)
+  mx(domain, "aspmx2.googlemail.com", 20)
+  mx(domain, "aspmx3.googlemail.com", 20)
+
+  -- Configure SPF
+  txt(domain, "v=spf1 a mx include:_spf.google.com ~all")
+end
